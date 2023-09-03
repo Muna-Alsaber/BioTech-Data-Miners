4a80c9750482c21b4e7796264e28df37dc9e46d0
Muna-Alsaber <98395910+Muna-Alsaber@users.noreply.github.com>
2023-09-04 01:46:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from xgboost import XGBRegressor
+import xgboost
+# from xgboost import XGBRegressor
 import pickle
 
 st.set_page_config(page_title="BioTech Data Miners", page_icon="🧊",)
